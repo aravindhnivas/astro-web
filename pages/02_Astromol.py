6c373f39a3deec26bcaa067c2e1afe472fd94902
Aravindh Nivas Marimuthu <aravindhnivas28@gmail.com>
2023-05-23 02:32:57 +0200

    Update 02_Astromol.py

--- pages/02_Astromol.py
+++ pages/02_Astromol.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import astromol as astro
 import plotly.express as px
+import pandas as pd
 
 st.set_page_config(page_title="Astromol", layout="wide")
 
@@ -18,33 +19,149 @@ def intro():
     
     """
     
-def telescope_summary(telescope):
+def telescope_summary(telescope: astro.Telescope):
 
-    # st.write('-'*len(telescope.name))
-    # st.write(f"{telescope.name}")
-    # st.write('-'*len(telescope.name))
-
-    st.write(f"{'Type':11}\t{telescope.type}")
-    if telescope.diameter:
-        st.write(f"{'Diameter':11}\t{telescope.diameter}")
-    st.write(f"{'Wavelengths':11}\t{', '.join(telescope.wavelength)}")
-    if telescope.latitude:
-        st.write(f"{'Latitude':11}\t{telescope.latitude}")
-    if telescope.longitude:
-        st.write(f"{'Longitude':11}\t{telescope.longitude}")
-    st.write(f"{'Built':11}\t{telescope.built}")
-    if telescope.decommissioned:
-        st.write(f"{'Decommissioned':11}\t{telescope.decommissioned}")
-
-    # mol_str = f"Molecules Detected With {telescope.name}"
-    # st.write("\n" + "-"*len(mol_str))
-    # st.write(mol_str)
-    # st.write("-"*len(mol_str))
+    st.markdown(f"""
+        - Short Name: {telescope.shortname}
+        - Type: {telescope.type}
+        - Diameter: {telescope.diameter}
+        - Wavelengths: {', '.join(telescope.wavelength)}
+        - Latitude: {telescope.latitude}
+        - Longitude: {telescope.longitude}
+        - Built: {telescope.built}
+        - Decommissioned: {telescope.decommissioned}
+    """)
     
-
     detects = [x.formula for x in astro.all_molecules if telescope in x.telescopes]
     st.write(f"{len(detects)} molecules detected With {telescope.name}")
     st.write(', '.join(detects))
+    
+def molecule_refs_summary(molecule: astro.Molecule):
+    """
+    Prints a nicely formatted list of references and notes to the terminal.
+    """
+
+    l_refs: list[str] = molecule.l_refs.split(";")
+    d_refs: list[str] = molecule.d_refs.split(";")
+
+    if molecule.notes != None:
+        notes = molecule.notes.strip("*")
+
+    st.markdown("**Detection Reference(s)**")
+    strs = ""
+    for x in range(len(d_refs)):
+        strs += f"- {d_refs[x].strip()}\n"
+    
+    st.markdown(strs)
+
+    st.markdown("**Laboratory Reference(s)**")
+    
+    strs = ""
+    for x in range(len(l_refs)):
+        strs += f"- {l_refs[x].strip()}\n"
+    st.markdown(strs)
+    
+    if molecule.notes != None:
+        st.write("_**Notes**_")
+        st.write(notes)
+
+    if molecule.isotopologues != None:
+        iso_d_refs = molecule.isos_d_refs.split("[")
+        del iso_d_refs[0]
+
+        st.write("**Isotopologue Detection Reference(s)**")
+        for x in iso_d_refs:
+            st.write("- [" + x.strip())
+
+    if molecule.ice == True or molecule.ice == "Tentative":
+        st.write("**Ice Reference(s)**")
+        # st.write("[Det] {}".format(molecule.ice_d_refs))
+        # st.write("[Lab] {}".format(molecule.ice_l_refs))
+        st.markdown(f"""
+            - [Det] {molecule.ice_d_refs}
+            - [Lab] {molecule.ice_l_refs}
+        """)
+
+    if molecule.ppd == True or molecule.ppd == "Tentative":
+        st.write("**Protoplanetary Disks Reference(s)**")
+        # st.write("[{}] {}".format(molecule.formula, molecule.ppd_d_refs))
+        strs = f"- [{molecule.formula}] {molecule.ppd_d_refs}\n"
+        
+        if molecule.ppd_isos != None:
+            for x in molecule.ppd_isos:
+                # st.write(f"[{x.formula}] {x.ppd_d_refs}")
+                strs += f"- [{x.formula}] {x.ppd_d_refs}\n"
+        st.markdown(strs)
+
+    if molecule.exgal == True or molecule.exgal == "Tentative":
+        st.write("**External Galaxies Reference(s)**")
+        st.write("- [{}] {}".format(molecule.formula, molecule.exgal_d_refs))
+
+    if molecule.exo == True or molecule.exo == "Tentative":
+        st.write("**Exoplanetary Atmospheres Reference(s)**")
+        # st.write("[{}] {}".format(molecule.formula, molecule.exo_d_refs))
+        strs = f"- [{molecule.formula}] {molecule.exo_d_refs}\n"
+        if molecule.exo_isos != None:
+            for x in molecule.exo_isos:
+                # st.write(f"[{x.formula}] {x.exo_d_refs}")
+                strs += f"- [{x.formula}] {x.exo_d_refs}\n"
+        st.markdown(strs)
+            
+            
+def molecule_summary(molecule):
+    """
+    Prints a summary of the information in the database for the molecule to the terminal.
+    """
+    
+    attr_lists = ["Neutral", "Cation", "Anion", "Cyclic", "Radical"]
+    attr_str = [attr_lists[ind] for ind, x in enumerate([molecule.neutral, molecule.cation, molecule.anion, molecule.cyclic, molecule.radical]) if x]
+    
+    st.markdown(f"""
+        ### {molecule.name} ({molecule.formula})
+        - Formula: {molecule.formula}
+        - Name: {molecule.name}
+        - Atoms: {molecule.natoms}
+        - Mass: {molecule.mass} amu
+        - Year Detected: {molecule.year}
+        - Source(s): {', '.join([x.name for x in molecule.sources])}
+        - Telescope(s) Used: {', '.join([x.shortname for x in molecule.telescopes])}
+        - Attributes: {', '.join(attr_str)}
+    """)
+    
+    other_envs = [molecule.ice, molecule.ppd, molecule.exgal, molecule.exo]
+    
+    other_str = ""
+    if any(other_envs) == True:
+        other_str = ""
+        if molecule.ice == True:
+            other_str += "Ices, "
+        if molecule.ice == "Tentative":
+            other_str += "Ices (Tentative), "
+        if molecule.ppd == True:
+            other_str += "Protoplanetary Disks, "
+        if molecule.ppd == "Tentative":
+            other_str += "Protoplanetary Disks (Tentative), "
+        if molecule.exgal == True:
+            other_str += "External Galaxies, "
+        if molecule.exgal == "Tentative":
+            other_str += "External Galaxies (Tentative), "
+        if molecule.exo == True:
+            other_str += "Exoplanetary Atmospheres, "
+        if molecule.exo == "Tentative":
+            other_str += "Exoplanetary Atmospheres (Tentative), "
+
+        other_str = other_str.strip().strip(",")
+
+
+    f"""
+        {(f'- Known Isotopologues: {molecule.isotopologues}' if molecule.isotopologues else "")}
+        {(f"- Also Detected In: {other_str}" if other_str else "")}
+        {(f'- Source(s) of External Galaxy Detections: {molecule.exgal_sources}' if molecule.exgal_sources else "")}
+        {(f'- Isotopologues Also Detected in Protoplanetary Disks: {",".join([x.formula for x in molecule.ppd_isos])}' if molecule.ppd_isos else "")}
+        {(f'- Isotopologues Also Detected in Exoplanetary Atmospheres: {",".join([x.formula for x in molecule.exo_isos])}' if molecule.exo_isos else "")}
+    """
+    
+    molecule_refs_summary(molecule)
     
 def get_telescopes():
     mainKey = f'updated: {astro.updated()}'
@@ -98,15 +215,17 @@ def get_telescopes():
 def main():
     intro()
     
-    telescopes_tab, molecular_catagories_tab = st.tabs(["Telescopes", "Molecular catagories"])
+    telescopes_tab, molecules_tab = st.tabs(["Telescopes", "Molecules"])
+    
+    total_molecules = len(astro.all_molecules)
     
     with telescopes_tab:
-        # st.subheader("Telescopes")
+        
         st.markdown(
             f"""
             ### Total number of telescopes: {len(astro.all_telescopes)}
             
-            - Detected {len(astro.all_molecules)} molecules in space.
+            - Detected {total_molecules} molecules in space.
             
             The following shows the number of telescopes in each wavelength range.
             
@@ -131,9 +250,34 @@ def main():
         # st.subheader(f"{selected_telescope_obj.name} summary")
         telescope_summary(selected_telescope_obj)
         
-    with molecular_catagories_tab:
-        st.subheader("Molecular catagories")
+    with molecules_tab:
         
+        st.subheader(f"Total number of molecules detected in space: {total_molecules}")
+        
+        neutrals = [mol.name for mol in astro.all_molecules if mol.neutral]
+        radicals = [mol.name for mol in astro.all_molecules if mol.radical]
+        cations = [mol.name for mol in astro.all_molecules if mol.cation]
+        cyclics = [mol.name for mol in astro.all_molecules if mol.cyclic]
+        anions = [mol.name for mol in astro.all_molecules if mol.anion]
+        fullerenes = [mol.name for mol in astro.all_molecules if mol.fullerene]
+        pahs = [mol.name for mol in astro.all_molecules if mol.pah]
+        
+        df = pd.DataFrame({
+            'neutrals': [len(neutrals), f"{(len(neutrals) / total_molecules * 100):.1f}"],
+            'radicals': [len(radicals), f"{(len(radicals) / total_molecules * 100):.1f}"],
+            'cations': [len(cations), f"{(len(cations) / total_molecules * 100):.1f}"],
+            'cyclics': [len(cyclics), f"{(len(cyclics) / total_molecules * 100):.1f}"],
+            'anions': [len(anions), f"{(len(anions) / total_molecules * 100):.1f}"],
+            'fullerenes': [len(fullerenes), f"{(len(fullerenes) / total_molecules * 100):.1f}"],
+            'PAHs': [len(pahs), f"{(len(pahs) / total_molecules * 100):.1f}"],
+        }, index=['count', 'percentage (%)'], dtype=float).T
+            
+        st.dataframe(df, use_container_width=True)
+        
+        st.subheader("Molecule summary")
+        selected_molecule = st.selectbox("Select a molecule", [_.name for _ in astro.all_molecules])
+        selected_molecule_obj = [_ for _ in astro.all_molecules if _.name == selected_molecule][0]
+        molecule_summary(selected_molecule_obj)
     
     about_page()
 
